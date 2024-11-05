ac5413a505dd3e7cf5e48cd9e9deaf27101aff66
Mikko Peltonen <mikko.inno.peltonen@gmail.com>
2024-11-05 16:33:42 +0200

    loading spinner ready

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -5,7 +5,7 @@ function LoadingSpinner() {
     return (
       <div className="items-center loading-spinner">
         <div className="spinner-border"></div>
-        <h5 className="text-center text-gray-600">Logging in...</h5>
+        <h5 className="text-center text-gray-600">Loading...</h5>
       </div>
     )
   }
