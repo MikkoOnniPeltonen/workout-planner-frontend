f2554949d5be0892ada9ecf5255b723af743ade5
Mikko Peltonen <mikko.inno.peltonen@gmail.com>
2024-11-05 18:00:52 +0200

    statistic component functionalities added

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,60 +1,65 @@
 
 import { Pie } from 'react-chartjs-2'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 ChartJS.register(ArcElement, Tooltip, Legend)
 
-function Statistics(workouts) {
+function Statistics({ workouts }) {
 
     const [chartWorkouts, setChartWorkouts] = useState([])
-    setChartWorkouts(workouts)
-
-    const muscleGroups = {
-        Legs: 0,
-        Back: 0,
-        Chest: 0,
-        'Upper back': 0,
-        Shoulders: 0,
-        Arms: 0,
-        Core: 0
-    }
-
-    chartWorkouts.forEach((workout) => {
-        workout.exercises.forEach((exercise) => {
-            if (muscleGroups[exercise]) {
-                muscleGroups[exercise] +1
-            }
-        })
+    const [data, setData] = useState({
+        labels: [],
+        datasets: [{
+            label: "Muscle Group Distribution",
+            data: [],
+            backgroundColor: [
+                '#FF6384', '#36A2EB', '#FFCE56',
+                '#4BC0C0', '#9966FF', '#FF9F40', '#8DD1E1'
+            ],
+        }]
     })
 
-    const data = {
-        labels: Object.keys(muscleGroups),
-        datasets: [
-            {
-                label: "Muscle Group Distribution",
+    useEffect(() => {
+
+        if (workouts && workouts.length > 0) {
+            setChartWorkouts(workouts)
+            updateMuscleGroups(workouts)
+        }
+    }, [workouts])
+
+    const updateMuscleGroups = (workouts) => {
+        const muscleGroups = {
+            Legs: 0,
+            Back: 0,
+            Chest: 0,
+            'Upper back': 0,
+            Shoulders: 0,
+            Arms: 0,
+            Core: 0,
+        }
+
+        workouts.forEach((workout) => {
+            workout.exercises.forEach((exercise) => {
+                if (muscleGroups.hasOwnProperty(exercise)) {
+                    muscleGroups[exercise]++
+                }
+            })
+        })
+
+        setData({
+            labels: Object.keys(muscleGroups),
+            datasets: [{
+                label: 'Muscle Group Distribution',
                 data: Object.values(muscleGroups),
                 backgroundColor: [
-                    '#FF6384',
-                    '#36A2EB',
-                    '#FFCE56',
-                    '#4BC0C0',
-                    '#9966FF',
-                    '#FF9F40',
-                    '#FF6384',
+                    '#FF6384', '#36A2EB', '#FFCE56',
+                    '#4BC0C0', '#9966FF', '#FF9F40', '#8DD1E1'
                 ],
-                hoverBackgroundColor: [
-                    '#FF6384',
-                    '#36A2EB',
-                    '#FFCE56',
-                    '#4BC0C0',
-                    '#9966FF',
-                    '#FF9F40',
-                    '#FF6384',
-                ],
-            }
-        ]
+            }]
+        })
     }
+    
   return (
     <div>
       <h2>Workout Muscle Group Statistics</h2>
