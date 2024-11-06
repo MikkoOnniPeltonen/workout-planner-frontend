617a4c92900220d834698c81494736786ba25c06
Mikko Peltonen <mikko.inno.peltonen@gmail.com>
2024-11-06 09:48:59 +0200

    tiny changes to display text

--- src/pages/AllExercisesPage.jsx
+++ src/pages/AllExercisesPage.jsx
@@ -119,7 +119,7 @@ function AllExercisesPage() {
                             <img src={motivationItems[currentMessage].icon} alt="Exercise icon" className="drawer-icon" />
                             <ul className="flex gap-10 list-disc pl-4">
                                 {selectedExercise.usedWith?.map((item, index) => (
-                                    <li key={index} className="text-sm m-4">{item}</li>
+                                    <li key={index} className="text-sm m-4">{item + ' '}</li>
                                 ))}
                             </ul>
                         </>
